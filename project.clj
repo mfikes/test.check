2d09d2e273ff2fdd352138f0b0b09de03ab4edb9
Mike Fikes <mike@fikesfarm.com>
2016-09-04 16:00:01 -0400

    Update to ClojureScript 1.9.227

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [org.clojure/clojurescript "1.7.48"]]}
              :self-host {:dependencies [[org.clojure/clojure "1.8.0"]
-                                        [org.clojure/clojurescript "1.9.14"]]
+                                        [org.clojure/clojurescript "1.9.227"]]
                          :main clojure.main}}
   :global-vars {*warn-on-reflection* true}
   :plugins [[lein-codox "0.9.1"]
